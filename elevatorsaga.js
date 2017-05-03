2142e56926ba0845f064e95d84433544c5749e01
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-05-03 10:57:00 -0500

    Level 4 partial solution

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,29 +1,31 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0];
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            console.log("idle");
-            //if not at top go one up
-            if (elevator.currentFloor() < floors.length-1){
-                console.log(elevator.destinationQueue);
-                elevator.goToFloor(elevator.currentFloor() + 1);
-            }
-            //if at top go to 0
-            else {
-                console.log(elevator.destinationQueue);
-                elevator.goToFloor(0);
-            }
-                
-            });
+        for (var e=0; e<elevators.length; e++){
+            console.log(e);
+            var elevator = elevators[e];
+            setElevatorHandlers(elevator);
+        }
+
+        function setElevatorHandlers(elevator){
+            elevator.on("idle", function() {
+                //if not at top go one up
+                if (elevator.currentFloor() < floors.length-1){
+                    elevator.goToFloor(elevator.currentFloor() + 1);
+                }
+                //if at top go to 0
+                else {
+                    elevator.goToFloor(0);
+                }
+                    
+                });
 
             elevator.on("floor_button_pressed", function(floorNum){
-                console.log("floor_button_pressed");
-                console.log(elevator.destinationQueue);
                 elevator.goToFloor(floorNum);
             });
+        }
 
     },
+
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
