0fd9200e8baf20b90c657d18a21b411d50f492b0
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-05-07 16:38:41 -0500

    checks other elevator queues before going to a floor; still bugged

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,7 +1,7 @@
 {
     init: function(elevators, floors) {
         var waitingQueue = [];
-        for (var e=0; e<elevators.length; e++){ //
+        for (var e=0; e<elevators.length; e++){ 
             var elevator = elevators[e];
             setElevatorHandlers(elevator);
         }
@@ -12,52 +12,43 @@
 
         function setFloorHandlers(floor){
             floor.on("up_button_pressed", function() {
-                //console.log("up_button_pressed BEFORE: ", waitingQueue, " length: ", waitingQueue.length);
                 if (!waitingQueue.includes(floor.level)){
                     waitingQueue.push(floor.level);
                 }
-
-                //console.log("up_button_pressed AFTER: ", waitingQueue, " length: ", waitingQueue.length);
-        })
+            })
             floor.on("down_button_pressed", function() {
-                //console.log("down_button_pressed BEFORE: ", waitingQueue, " length: ", waitingQueue.length)
                 if (!waitingQueue.includes(floor.level)){
                     waitingQueue.push(floor.level);
                 }
-                //console.log("down_button_pressed AFTER: ", waitingQueue, " length: ", waitingQueue.length);
-        })
+            })
         }
 
         function setElevatorHandlers(elevator){
             elevator.on("idle", function() {
                 //if not at top go one up;
-                console.log("idle: ");//, waitingQueue, " length: ", waitingQueue.length);
                 if (waitingQueue.length > 0){
-                    elevator.goToFloor(waitingQueue[0]);
+                    if (!in_another_queue(waitingQueue[0])){
+                        elevator.goToFloor(waitingQueue[0]);
+                    }
+                    
                 }
                 else{
                     var assignedFloor = elevators.indexOf(elevator);
                     elevator.goToFloor(assignedFloor);
-                    //elevator.goToFloor(Math.floor(floors.length/2));
                 }
                 setElevatorSignals(elevator);
                 });
 
             elevator.on("floor_button_pressed", function(floorNum){
-                //console.log("floor_button_pressed");
                 elevator.goToFloor(floorNum);
-
                 optimizeOrder(elevator);
                 setElevatorSignals(elevator);
             });
 
             elevator.on("stopped_at_floor", function(floorNum) {
                 if (waitingQueue.includes(floorNum)){
-                    //console.log("This floor is in our waitingQueue!!", waitingQueue);
                     var index = waitingQueue.indexOf(floorNum);
-                    //console.log("INDEX YO:", index);
                     waitingQueue.splice(index, 1);
-                    //console.log("EXTERMINATE!!", waitingQueue);
                 }
 
                 optimizeOrder(elevator);
@@ -70,8 +61,10 @@
                     //person also needs to be going same direction
                     if ((elevator.goingDownIndicator() && direction == "up") || elevator.goingDownIndicator() && direction == "down"){
                         //stop and pick them up
-                        elevator.goToFloor(floorNum);
-                        optimizeOrder(elevator);
+                         if (!in_another_queue(waitingQueue[0])){
+                            elevator.goToFloor(floorNum);
+                            optimizeOrder(elevator);
+                        }
                     }
 
                 }
@@ -100,13 +93,22 @@
         function optimizeOrder(elevator){
             //need to optimize destination queue not pressed floors
              if (elevator.goingUpIndicator() && !elevator.goingDownIndicator()){
-                elevator.destinationQueue = elevator.destinationQueue.sort()//elevator.getPressedFloors();
+                elevator.destinationQueue = elevator.destinationQueue.sort();
                 elevator.checkDestinationQueue();
             }
             if (elevator.goingDownIndicator() && !elevator.goingUpIndicator()){
-                elevator.destinationQueue = elevator.destinationQueue.sort().reverse()//elevator.getPressedFloors().reverse();
+                elevator.destinationQueue = elevator.destinationQueue.sort().reverse();
                 elevator.checkDestinationQueue();  
             }
+      }
+
+      function in_another_queue(floorNum){
+        for (e=0; e<elevators.length; e++){ 
+            if (elevators[e].destinationQueue.includes(floorNum)){
+                return true;
+            }
+        }
+        return false;
       }
 
 },
